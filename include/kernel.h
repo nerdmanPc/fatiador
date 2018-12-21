661596bd0767286d8541854c61a0e67eea90f07f
Pedro Costa <nerdmannnn@gmail.com>
2018-12-21 19:25:43 -0300

    inicio da definição de interfaces 2

--- include/kernel.h
+++ include/kernel.h
@@ -2,11 +2,24 @@ typedef long Integer;
 typedef double Rational;
 typedef double Real;
 
-typedef struct{
-    Real x, y, z;
-} Real_3;
+struct Real_3{
+  Real x, y, z;
+};
+
+struct Real_2{
+  Real x, y;
+};
 
 struct Vertex_3{
   Real_3 position;
   Real_3 normal;
 };
+
+struct Vertex_2{
+  Real_2 position;
+  Real_2 normal;
+};
+
+struct Polygon_2{
+    
+}
